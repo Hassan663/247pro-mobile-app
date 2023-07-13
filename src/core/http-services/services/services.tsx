3a328a3d4673447838269f07e91ef987f48c5a6c
muzammil hussain shah <mynameismuzammilhussainshah@gmail.com>
2023-07-13 22:35:45 +0500

    postApi spell [update]

--- src/core/http-services/services/services.tsx
+++ src/core/http-services/services/services.tsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const postAi = async (url: string, postData: object) => { return await axios.post(url, postData) };
+const postApi = async (url: string, postData: object) => { return await axios.post(url, postData) };
 const getApi = async (url: any) => { return await axios.get(url) };
 const deleteAp = async (url: string) => { return await axios.delete(url) };
 const updateApi = async (url: string, postData: object) => { return await axios.put(url, postData) };
 
 
-export { postAi, getApi, deleteAp, updateApi };
+export { postApi, getApi, deleteAp, updateApi };
--- src/store/action/action.tsx
+++ src/store/action/action.tsx
@@ -1,6 +1,6 @@
 import ActionTypes from "../constant/constant";
 import { CommonActions } from '@react-navigation/native';
-import { getApi } from '../../core/http-services/services/services';
+import { postApi } from '../../core/http-services/services/services';
 import { LOGIN_ENDPOINT } from '../../core/http-services/apis/apis';
 import { Dispatch } from 'redux';
 import { IResponse } from '../../core/modals';
@@ -9,12 +9,16 @@ import { IResponse } from '../../core/modals';
 export const login = () => {
     return async (dispatch: Dispatch) => {
         try {
-            let userData = getApi(LOGIN_ENDPOINT)
-            let loginResponse: IResponse<typeof userData>;
 
+            // loader will apear
+            let userData = postApi(LOGIN_ENDPOINT, { phone: '123', pass: 'xyz' })
+            let loginResponse: IResponse<typeof userData>;
+            // data will be save in redux store
+            // loader will close
 
 
         } catch (error) {
+            // if something is wrong error will save in store and will show the error here
             console.log(error)
         }
     }
