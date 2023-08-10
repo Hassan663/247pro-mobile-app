8673a9e3662fcaa9d1edb3f2e2da84c01905c744
muzammil hussain shah <mynameismuzammilhussainshah@gmail.com>
2023-08-10 20:08:43 +0500

    code format

--- src/pages/auth/forget-password/forget-password.pages.tsx
+++ src/pages/auth/forget-password/forget-password.pages.tsx
@@ -1,34 +1,23 @@
 // @app
-import React, { useState } from 'react';
+import React from 'react';
 import {
     View,
-    Image,
     ScrollView,
     TouchableOpacity,
     Dimensions,
 } from 'react-native';
 
-import CountryPicker, {
-    Country,
-} from 'react-native-country-picker-modal';
-import AntDesign from 'react-native-vector-icons/AntDesign';
-import { RFPercentage } from 'react-native-responsive-fontsize';
+import { t } from 'i18next';
 
 import {
     FooterText,
-    ScreenSubTitle,
     ScreenTitle,
     Title,
 } from '../../../core/components/screen-title.component';
-import { styles } from './forget-password.style';
 import Colors from '../../../styles/colors';
 import Input from '../../../core/components/input.component';
 import Button from '../../../core/components/button.component';
-import { changeRoute, setItem } from '../../../core/helpers/async-storage';
-import { t } from 'i18next';
-import { appLanguages } from '../../../utilities/languageData';
-import i18n, { fetchTranslations } from '../../../i18n';
-import { useSelector } from 'react-redux';
+import { styles } from './forget-password.style';
 
 const windowHeight = Dimensions.get('window').height;
 const heightFlex1 = windowHeight / 10;
@@ -57,16 +46,13 @@ const ForgetPassword: React.FC<{ navigation: any }> = ({ navigation }) => {
 
                 <View style={[styles.logInBtnContainer,]}>
                     <View />
-                    <Button
-                        // callBack={() => { changeRoute(navigation, 'VerifyCode') }}
-                        title={t(`Reset_Password`)} primary />
+                    <Button title={t(`Reset_Password`)} primary />
                 </View>
 
                 <View style={styles.footerContainer}>
                     <View style={styles.footerTextWrapper}>
                         <FooterText color={Colors.fontColor} title={t('Remember_you_password')} />
                         <TouchableOpacity
-                            // onPress={() => changeRoute(navigation, 'SignUp')}
                             activeOpacity={0.8}>
                             <FooterText color={Colors.primary} title={t('logintText')} />
                         </TouchableOpacity>
--- src/pages/auth/forget-password/forget-password.style.tsx
+++ src/pages/auth/forget-password/forget-password.style.tsx
@@ -4,13 +4,11 @@ import Colors from '../../../styles/colors';
 
 
 export const styles = StyleSheet.create({
-
     container: {
         flex: 1,
         paddingHorizontal: RFPercentage(2.5),
         backgroundColor: Colors.white
     },
-
     titleWrapper: {
         flexDirection: 'row',
         flex: 2,
@@ -35,53 +33,12 @@ export const styles = StyleSheet.create({
     inputWrapper: {
         flexDirection: 'row',
         alignItems: "center",
-        marginVertical:RFPercentage(2)
-    },
-    flagContainer: {
-        width: '15%',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: "center",
-        height: 40
-    },
-    phoneNumberInput: {
-        width: "85%"
-    },
-    socialButtonContainer: {
-        height: 48,
-        width: "100%",
-        flexDirection: "row",
-        marginVertical: RFPercentage(1.5),
-        borderWidth: 1,
-        borderRadius: RFPercentage(1),
-        borderColor: Colors.lightGray,
-        justifyContent: "center",
-        alignItems: 'center'
-    },
-    socialText: {
-        fontSize: RFPercentage(2),
-        letterSpacing: RFPercentage(.2),
-        fontWeight: '700',
+        marginVertical: RFPercentage(2)
     },
     footerTextWrapper: {
         flexDirection: "row",
         justifyContent: "center",
         alignItems: 'center'
     },
-    flagWrapper: {
-        height: 30,
-        width: 30
-    },
-    downIcon: { marginHorizontal: RFPercentage(.5) },
-    orContainer: {
-        flexDirection: 'row',
-        height: 20,
-        marginVertical: RFPercentage(3),
-        justifyContent: 'center',
-        alignItems: "center"
-    },
-    googleIcon: {
-        height: RFPercentage(4.5),
-        width: RFPercentage(4.5)
-    },
+
 });
