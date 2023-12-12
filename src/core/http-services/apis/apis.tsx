3574db2001bb9a51b0d3f187d477eb34544070d9
Muzammil Hussain Shah <mynameismuzammilhussainshah@gmail.com>
2023-12-13 01:52:00 +0500

    Base URL [UPDATE]

--- src/core/http-services/apis/apis.tsx
+++ src/core/http-services/apis/apis.tsx
@@ -13,13 +13,13 @@ const createEndpointWithoutBaseUrl = (endpoint: string, JWTToken: boolean): Endp
 });
 
 // AUTHENTICATION ENDPOINTS
-export const LOGIN_ENCRIPTION_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl('https://apidevprofinder.247pro.com/api/system/encryption/encode', false);
-export const LOGIN_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl('https://apidevprofinder.247pro.com/api/auth/login', false);
-export const SIGNUP_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl('https://apidevprofinder.247pro.com/api/auth/register', false);
-export const MEMBERSHIP_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl('https://apidevprofinder.247pro.com/membership-api/v1/accounts', false);
-export const LOGIN_IDENTITY_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl('https://apidevprofinder.247pro.com/api/account/identity', false);
-export const FORGET_PASSWORD_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl('https://apidevprofinder.247pro.com/api/auth/forgot-password', false);
-export const LOGOUT_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl('https://apidevprofinder.247pro.com/api/auth/logout', false);
+export const LOGIN_ENCRIPTION_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${BASE_URL}/api/system/encryption/encode`, false);
+export const LOGIN_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${BASE_URL}/api/auth/login`, false);
+export const SIGNUP_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${BASE_URL}/api/auth/register`, false);
+export const MEMBERSHIP_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${BASE_URL}/membership-api/v1/accounts`, false);
+export const LOGIN_IDENTITY_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${BASE_URL}/api/account/identity`, false);
+export const FORGET_PASSWORD_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${BASE_URL}/api/auth/forgot-password`, false);
+export const LOGOUT_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${BASE_URL}/api/auth/logout`, false);
 export const USER_ENDPOINT: Endpoint = createEndpoint('/api/user', true);
 export const POST_ENDPOINT: Endpoint = createEndpoint('/api/posts', true);
 export const CHECKOUT_ENDPOINT: Endpoint = createEndpoint('/api/checkout', true);
@@ -29,8 +29,8 @@ export const BIDCOST_ENDPOINT: Endpoint = createEndpoint('/api/bidcost', true);
 // AUTHENTICATION ENDPOINTS
 
 // APP ENDPOINTS
-export const CONTACT_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl('https://apidevprofinder.247pro.com/application-api/v1/contacts', false);
-export const CREATE_CONTACT_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl('https://apidevprofinder.247pro.com/application-api/v1/contacts', false);
+export const CONTACT_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${BASE_URL}/application-api/v1/contacts`, false);
+export const CREATE_CONTACT_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${BASE_URL}/application-api/v1/contacts`, false);
 // APP ENDPOINTS
 
 // Rest of the code remains the same
