78b18854a6437224bf99fc5c2a19d3a2944f5ac1
Muzammil Hussain Shah <35377074+muzammilhussainshah@users.noreply.github.com>
2024-09-12 23:16:54 +0500

    api endpoint [UPDATE]

--- src/core/http-services/apis/apis.tsx
+++ src/core/http-services/apis/apis.tsx
@@ -4,22 +4,24 @@ import BASE_URL from '../base/base-url';
 // Interface for an endpoint
 // Function to create an endpoint object with JWTToken property
 const createEndpoint = (endpoint: string, JWTToken: boolean): Endpoint => ({
-  url: `${BASE_URL}${endpoint}`,
+  url: `${'https://api.247pro.com'}${endpoint}`,
   JWTToken,
 });
 const createEndpointWithoutBaseUrl = (endpoint: string, JWTToken: boolean): Endpoint => ({
   url: `${endpoint}`,
   JWTToken,
 });
-console.log(BASE_URL,'BASE_URL')
+console.log(BASE_URL, 'BASE_URL22222')
+
 // AUTHENTICATION ENDPOINTS
-export const LOGIN_ENCRIPTION_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${BASE_URL}/api/system/encryption/encode`, false);
-export const LOGIN_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${BASE_URL}/api/auth/login`, false);
-export const SIGNUP_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${BASE_URL}/api/auth/register`, false);
-export const MEMBERSHIP_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${BASE_URL}/membership-api/v1/accounts`, false);
-export const LOGIN_IDENTITY_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${BASE_URL}/api/account/identity`, false);
-export const FORGET_PASSWORD_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${BASE_URL}/api/auth/forgot-password`, false);
-export const LOGOUT_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${BASE_URL}/api/auth/logout`, false);
+export const LOGIN_ENCRIPTION_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${'https://api.247pro.com'}/api/system/encryption/encode`, false);
+export const LOGIN_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${'https://api.247pro.com'}/api/auth/login`, false);
+// export const LOGIN_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${'https://api.247pro.com'}/api/auth/login`, false);
+export const SIGNUP_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${'https://api.247pro.com'}/api/auth/register`, false);
+export const MEMBERSHIP_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${'https://api.247pro.com'}/membership-api/v1/accounts`, false);
+export const LOGIN_IDENTITY_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${'https://api.247pro.com'}/api/account/identity`, false);
+export const FORGET_PASSWORD_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${'https://api.247pro.com'}/api/auth/forgot-password`, false);
+export const LOGOUT_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${'https://api.247pro.com'}/api/auth/logout`, false);
 export const USER_ENDPOINT: Endpoint = createEndpoint('/api/user', true);
 export const POST_ENDPOINT: Endpoint = createEndpoint('/api/posts', true);
 export const CHECKOUT_ENDPOINT: Endpoint = createEndpoint('/api/checkout', true);
@@ -29,9 +31,9 @@ export const BIDCOST_ENDPOINT: Endpoint = createEndpoint('/api/bidcost', true);
 // AUTHENTICATION ENDPOINTS
 
 // APP ENDPOINTS
-export const CONTACT_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${BASE_URL}/application-api/v1/contacts`, false);
-export const CONTACT_SPECIALITIES: Endpoint = createEndpointWithoutBaseUrl(`${BASE_URL}/membership-api/v1/specialities`, false);
-export const CREATE_CONTACT_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${BASE_URL}/application-api/v1/contacts`, false);
-export const UPLOAD_IMAGE_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${BASE_URL}/api/contacts/upload/profile-image`, false);
+export const CONTACT_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${'https://api.247pro.com'}/application-api/v1/contacts`, false);
+export const CONTACT_SPECIALITIES: Endpoint = createEndpointWithoutBaseUrl(`${'https://api.247pro.com'}/membership-api/v1/specialities`, false);
+export const CREATE_CONTACT_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${'https://api.247pro.com'}/application-api/v1/contacts`, false);
+export const UPLOAD_IMAGE_ENDPOINT: Endpoint = createEndpointWithoutBaseUrl(`${'https://api.247pro.com'}/api/contacts/upload/profile-image`, false);
 // APP ENDPOINTS
 // Rest of the code remains the same
